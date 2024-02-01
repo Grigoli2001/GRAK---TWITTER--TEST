883fbc8eb022f115e5d703dd3c1a0221b2832edc
pyrees011 <raunakbhansali@Raunaks-MacBook-Air.local>
2024-02-01 01:01:13 +0100

    settings Page

--- front/src/App.js
+++ front/src/App.js
@@ -1,58 +1,72 @@
 import LoginPage from "./pages/LoginPage";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
-import Feed from "./pages/Feed";
-import Explore from "./pages/Explore";
-import Profile from "./pages/Profile";
-import Messages from "./pages/Messages";
-import Notifications from "./pages/Notifications";
-import Bookmarks from "./pages/Bookmarks";
-import NotFound from "./pages/NotFound/NotFoundSearch";
-import TestLayout from "./components/layouts/TestLayout";
-import PrivateRoute from "./routes/PrivateRoute";
-import PublicRoute from "./routes/PublicRoute";
+import Feed from './pages/Feed';
+import Explore from './pages/Explore';
+import Profile from './pages/Profile';
+import Messages from './pages/Messages';
+import Notifications from './pages/Notifications';
+import Bookmarks from './pages/Bookmarks';
+import NotFound from './pages/NotFound/NotFoundSearch';
+import AccountSettings from "./pages/Settings/AccountSettings";
+import SecuritySettings from "./pages/Settings/SecuritySettings";
+import PrivacySettings from "./pages/Settings/PrivacySettings";
+import NotificationSettings from "./pages/Settings/NotificationSettings";
+import AccessibilitySettings from "./pages/Settings/AccessibilitySettings";
+import MonetizationSettings from "./pages/Settings/MonetizationSettings";
+import CreatorSettings from "./pages/Settings/CreatorSettings";
+import TestLayout from './components/layouts/TestLayout';
+import SettingsLayout from './components/layouts/SettingsLayout';
 
-// test
+// test 
 
 function App() {
   return (
-    <Routes>
-      <Route element={<PublicRoute />}>
-        <Route path="/login" element={<LoginPage />} />
-      </Route>
-      <Route element={<PrivateRoute />}>
-        <Route element={<TestLayout />}>
-          <Route path="/home" element={<Feed />} />
+          <Routes>
+              <Route path="/" element={<LoginPage />} />
 
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/bookmarks" element={<Bookmarks />} />
-          <Route path=":username" element={<Profile />}>
-            {/* photo */}
-            {/* cover */}
-            {/* bio */}
-            {/* followers */}
-            {/* following */}
-          </Route>
-        </Route>
-      </Route>
+                <Route element={<TestLayout/>}>
+                    <Route path="/home" element={<Feed />} />
 
-      <Route
-        element={
-          <TestLayout excludeRenderWidgets={["SearchBar", "Trending"]} />
-        }
-      >
-        <Route path="/explore">
-          <Route index element={<Explore />} />
-          {/* any search features */}
-        </Route>
-      </Route>
+                    <Route path="/notifications" element={<Notifications />} />
+                    <Route path="/bookmarks" element={<Bookmarks />} />
+                    <Route path=":username" element={<Profile />}>
+                      {/* photo */}
+                      {/* cover */}
+                      {/* bio */}
+                      { /* followers */}
+                      { /* following */}
+                    </Route>
+                </Route>
 
-      <Route element={<TestLayout />}>
-        <Route path="/messages" element={<Messages />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+                <Route element={<TestLayout excludeRenderWidgets={['SearchBar', 'Trending']}/>}>
+                    <Route path="/explore" >
+                          <Route index element={<Explore />} />
+                          {/* any search features */}
+                        </Route>
+                </Route>
+
+                <Route element={<TestLayout/>}>
+                  <Route path="/messages" element={<Messages />} />
+                </Route>
+
+                <Route element={<SettingsLayout/>}>
+                  <Route path="/settings" >
+                    <Route index element={<Navigate to="account" replace />} />
+                    <Route path="account" element={<AccountSettings />} />
+                    <Route path="security" element={<SecuritySettings />} />
+                    <Route path="privacy" element={<PrivacySettings />} />
+                    <Route path="notifications" element={<NotificationSettings />} />
+                    <Route path="accessibility" element={<AccessibilitySettings />} />
+                    <Route path="monetization" element={<MonetizationSettings />} />
+                    <Route path="creator" element={<CreatorSettings />} />
+                  </Route>
+                </Route>
+                <Route path="*" element={<NotFound />} />
+          </Routes>
   );
+
 }
 
 export default App;
